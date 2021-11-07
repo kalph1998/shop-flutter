642263b9c725a79e9953956f2134f5aa79d61992
kalph <kalpupadhyay@gmail.com>
2021-11-07 18:19:15 +0530

    will add authorization

--- lib/screens/orders_screen.dart
+++ lib/screens/orders_screen.dart
@@ -13,40 +13,44 @@ class OrdersScreen extends StatefulWidget {
 }
 
 class _OrdersScreenState extends State<OrdersScreen> {
-  var _isLoading = false;
 
-  @override
-  void initState() {
-    Future.delayed(Duration.zero).then( (_) async {
-     setState(() {
-       _isLoading = true;
-     });
-     await  Provider.of<Orders>(context, listen: false).fetchAndSetOrders();
-
-     setState(() {
-       _isLoading = false;
-     });
-    } );
-    super.initState();
-  }
 
   @override
   Widget build(BuildContext context) {
-    final order = Provider.of<Orders>(context);
+    // final order = Provider.of<Orders>(context);
 
     return Scaffold(
-      drawer: AppDrawer(),
-      appBar: AppBar(
-        title: const Text('Your Orders'),
-      ),
-      body: _isLoading ? const Center(child:  CircularProgressIndicator(),) : ListView.builder(
-        itemBuilder: (ctx, index) {
-          return OrderItemWidget(
-            order: order.orders[index],
-          );
-        },
-        itemCount: order.orders.length,
-      ),
-    );
+        drawer: AppDrawer(),
+        appBar: AppBar(
+          title: const Text('Your Orders'),
+        ),
+        body: FutureBuilder(
+          future:
+              Provider.of<Orders>(context, listen: false).fetchAndSetOrders(),
+          builder: (ctx, dataSnapshot) {
+            if (dataSnapshot.connectionState == ConnectionState.waiting) {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            } else {
+              if (dataSnapshot.error != null) {
+                return const Center(
+                  child: Text('An error occured'),
+                );
+              } else {
+                return Consumer<Orders>(
+                  builder: (ctx, order, child) => ListView.builder(
+                    itemBuilder: (ctx, index) {
+                      return OrderItemWidget(
+                        order: order.orders[index],
+                      );
+                    },
+                    itemCount: order.orders.length,
+                  ),
+                );
+              }
+            }
+          },
+        ));
   }
 }
